ebeb7fecb2b0286845ce3ac014731a97de2d8f0c
KamuKamlesh08 <60519237+KamuKamlesh08@users.noreply.github.com>
2025-08-31 02:30:35 +0530

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 export const introspectHandler = (req, res) => {
   const incomingAuth = req.headers.authorization;
-
+  console.log("Received Authorization header:", incomingAuth);
   if (!incomingAuth) {
     return res.status(401).json({
       error: "Authorization header is missing",
@@ -13,9 +13,11 @@ export const introspectHandler = (req, res) => {
     });
   }
 
+  console.log("Received Authorization header:", incomingAuth);
+
   return res.status(200).json({
     active: true,
-    scope: "view_products:project_key",
+    scope: "view_products:captain-midnight",
     sub: "user-12345",
     exp: Math.floor(Date.now() / 1000) + 3600,
     iss: "https://your-introspect-server.com",
